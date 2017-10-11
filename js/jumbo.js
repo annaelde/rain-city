ee78c6fafe4611ca39d4f20ccce0fed7bec24e52
annaelde <elde.av@gmail.com>
2017-10-11 19:32:21 -0400

    modified hero image to use same script as other hero images.

--- js/jumbo.js
+++
@@ -1,6 +0,0 @@
-// Enables parallax on jumbrotron image when scrolling
-$(window).scroll((e) => {
-    var jumboHeight = $('.jumbotron').outerHeight();
-    var scrollDistance = $(window).scrollTop();
-    $('.bg').css('height', (jumboHeight - scrollDistance) + 'px');
-});
