260c7afe4ba4c73fb1239197764758072cabb3e0
annaelde <elde.av@gmail.com>
2017-10-11 19:32:29 -0400

    does not run on mobile now.

--- js/parallax.js
+++ js/parallax.js
@@ -1,10 +1,18 @@
 // Enables parallax on hero image when scrolling
-$(window).scroll(function(e) {
-    if ($('.hero').offset().top < $(window).scrollTop() + 56) {
-        var distance = $(window).scrollTop() - $('.hero').offset().top
-        var half = distance + 'px'
-        $('.hero').find('img').css('top', half)
-    } else {
-        $('.hero').find('img').css('top', '-56px')
-    }
-})
+if (!/Mobi/.test(navigator.userAgent)) {
+    $(window).scroll(function(e) {
+        requestAnimationFrame(function() {
+            if ($('.hero').offset().top < $(window).scrollTop() + 56) {
+                var distance = $(window).scrollTop() - $('.hero').offset().top
+                var half = distance + 'px'
+                $('.hero')
+                    .find('img')
+                    .css('top', half)
+            } else {
+                $('.hero')
+                    .find('img')
+                    .css('top', '-56px')
+            }
+        })
+    })
+}
