4f48cfbe6977591290252ae231f0c922a85d61d2
annaelde <elde.av@gmail.com>
2017-10-11 20:35:38 -0400

    modified formatting.

--- js/parallax.js
+++ js/parallax.js
@@ -1,18 +1,14 @@
 // Enables parallax on hero image when scrolling
+
+// Only assign event listener on desktop
 if (!/Mobi/.test(navigator.userAgent)) {
     $(window).scroll(function(e) {
         requestAnimationFrame(function() {
-            if ($('.hero').offset().top < $(window).scrollTop() + 56) {
+            if ($('.hero').offset().top < $(window).scrollTop()) {
                 var distance = $(window).scrollTop() - $('.hero').offset().top
                 var half = distance + 'px'
-                $('.hero')
-                    .find('img')
-                    .css('top', half)
-            } else {
-                $('.hero')
-                    .find('img')
-                    .css('top', '-56px')
-            }
+                $('.hero img').css('top', half)
+            } else $('.hero img').css('top', '0')
         })
     })
 }
